28090bff617d1ab24ab8a3df5d1ee32881babe8e
Leandiv <leandro.divito@gmail.com>
2020-09-25 01:27:16 -0300

    Part 1 - Exercise 1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -4,29 +4,44 @@ import ReactDOM from 'react-dom'
 
 const Button = ({handler, name}) => <button onClick={handler}>{name}</button>
 
+const Statics = ({feedback}) => {
+  const good = feedback[0].value;
+  const neutral = feedback[1].value;
+  const bad = feedback[2].value;
+
+  return (
+    <div>
+      <h1>Statics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {good + neutral + bad}</p>
+      <p>Average: {(good - bad) / (good + neutral + bad)}</p>
+      <p>Positive: {(good / (good + neutral + bad)) * 100} %</p>
+    </div>
+  )
+}
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [avg, setAvg] = useState(0)
+
+  const feedback = [
+    {name: "Good", value: good},
+    {name: "Neutral", value: neutral},
+    {name: "Bad", value: bad}
+  ]
 
   const handleGoodFeedback = () => {
     setGood(good + 1)
-    setAll(all + 1)
-    setAvg(avg + 1)
   }
   const handleNeutralFeedback = () => {
     setNeutral(neutral + 1)
-    setAll(all + 1)
-    setAvg(avg + 0)
   }
   const handleBadFeedback = () => {
     setBad(bad + 1)
-    setAll(all + 1)
-    setAvg(avg - 1)
   }
 
   return (
@@ -35,13 +50,7 @@ const App = () => {
       <Button handler={handleGoodFeedback} name="Good" />
       <Button handler={handleNeutralFeedback} name="Neutral" />
       <Button handler={handleBadFeedback} name="Bad" />
-      <h1>Statics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {all}</p>
-      <p>Average: {avg / all}</p>
-      <p>Positive: {(good / all) * 100} %</p>
+      <Statics feedback={feedback} />
     </div>
   )
 }
