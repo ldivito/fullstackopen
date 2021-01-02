5148bc9a134d5d40ceae2754fc5a7d52a585a12a
Leandiv <leandro.divito@gmail.com>
2021-01-02 18:07:23 -0300

    Part 2 - Exercise 2.8

--- part2/phonebook/src/app.js
+++ part2/phonebook/src/app.js
@@ -2,26 +2,32 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ])
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
-    console.log(checkDuplicate(persons, newName))
     if (checkDuplicate(persons, newName)) {
       alert(`${newName} is already added to phonebook`)
       return
     }
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNewPerson = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const checkDuplicate = (arr, val) => {
@@ -35,7 +41,8 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNewPerson} />
+          <div>name: <input value={newName} onChange={handleNewPerson} /></div>
+          <div>number: <input value={newNumber} onChange={handleNewNumber} /></div>
         </div>
         <div>
           <button type="submit">add</button>
@@ -43,7 +50,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => <li key={person.name}>{person.name}</li>)}
+        {persons.map((person) => <li key={person.name}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   )
