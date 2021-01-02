e08487e399a39d361e026855fe3daf12f155da66
Leandiv <leandro.divito@gmail.com>
2021-01-02 18:32:18 -0300

    Part 2 - Exercise 2.9

--- part2/phonebook/src/app.js
+++ part2/phonebook/src/app.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ newNameFilter, setNewNameFilter ] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -30,6 +34,10 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleNameFilter = (event) => {
+    setNewNameFilter(event.target.value)
+  }
+
   const checkDuplicate = (arr, val) => {
     return arr.some(function(arrVal) {
       return val === arrVal.name;
@@ -39,6 +47,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>Filter shown with: <input value={newNameFilter} onChange={handleNameFilter} /></div>
+      <h3>Add new contact</h3>
       <form onSubmit={addPerson}>
         <div>
           <div>name: <input value={newName} onChange={handleNewPerson} /></div>
@@ -48,9 +58,9 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h3>Numbers</h3>
       <ul>
-        {persons.map((person) => <li key={person.name}>{person.name} {person.number}</li>)}
+        {persons.filter(value => value.name.toLowerCase().match(newNameFilter.toLowerCase())).map((person) => <li key={person.name}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   )
