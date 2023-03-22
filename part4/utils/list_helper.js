3b5829bdca118cba188fb87a84e8c1ab8e9c9b25
Leandiv <leandro.divito@gmail.com>
2023-03-22 20:14:15 -0300

    Exercise 4.5: Favorite blog unit test

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -4,7 +4,24 @@ const totalLikes = blogs => (
 	blogs.reduce((sum, blog) => sum + blog.likes, 0)
 )
 
+const favouriteBlog = blogs => {
+	let max = 0
+	let favourite = null
+	blogs.map((blog) => {
+		if (blog.likes > max) {
+			max = blog.likes
+			favourite = {
+				title: blog.title,
+				author: blog.author,
+				likes: blog.likes
+			}
+		}
+	})
+	return favourite
+}
+
 module.exports = {
 	dummy,
-	totalLikes
+	totalLikes,
+	favouriteBlog
 }
