8bf4e91305cb87fa870d7d1e43a219918b7a61ed
Leandiv <leandro.divito@gmail.com>
2023-03-22 19:53:49 -0300

    Exercise 4.4: Test blog list total likes + Lint

--- part4/index.js
+++ part4/index.js
@@ -1,9 +1,10 @@
-require('http');
+require('http')
 const app = require('./app') // the actual Express application
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+	logger.info(`Server running on port ${config.PORT}`)
 })
