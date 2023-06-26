782631fe856265c012b3ba1ab26eec3d4674dc7e
Leandiv <leandro.divito@gmail.com>
2023-06-26 09:12:17 -0300

    Part 5 - Exercise 5.7 - Add blog details

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,40 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import {useState} from "react";
+
+const Blog = ({ blog }) => {
+
+  const [blogVisible, setBlogVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const hideWhenVisible = { display: blogVisible ? 'none' : '' }
+  const showWhenVisible = { display: blogVisible ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <span style={hideWhenVisible}>
+           <button onClick={() => setBlogVisible(true)}>View</button>
+        </span>
+        <span style={showWhenVisible}>
+          <button onClick={() => setBlogVisible(false)}>Hide</button>
+        </span>
+      </div>
+
+      <div style={showWhenVisible}>
+        <ul>
+          <li>URL: {blog.url}</li>
+          <li>Likes: {blog.likes}</li>
+          <li>User: {blog.user.username}</li>
+        </ul>
+      </div>
+    </div>
+)}
 
 export default Blog
