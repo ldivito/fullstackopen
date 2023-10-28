0e6ae97d54aeedaaff04182116efaa3c215d7706
Leandiv <leandro.divito@gmail.com>
2023-10-28 15:36:18 -0300

    Part 6 - Exercise 6.8 - Separate anecdote list to its own component

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,36 @@
+import {useDispatch, useSelector} from "react-redux";
+import {addVote} from "../reducers/anecdoteReducer";
+
+const Anecdote = ({ anecdote, handleClick }) => {
+  return (
+    <div>
+      {anecdote.content}
+      <div>
+        has {anecdote.votes} &nbsp;
+        <button onClick={handleClick}>vote</button>
+      </div>
+    </div>
+  )
+}
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+
+  const vote = (id) => {
+    dispatch(addVote(id))
+  }
+
+  return (
+    <>
+      {anecdotes.map(anecdote =>
+        <Anecdote key={anecdote.id}
+          anecdote={anecdote}
+          handleClick={() => vote(anecdote.id)}
+        />
+      )}
+    </>
+  )
+}
+
+export default AnecdoteList
