aa3d0427407c63e3b99c9b3be90a3511ab9c7387
Leandiv <leandro.divito@gmail.com>
2023-10-30 17:38:32 -0300

    Part 6 - Exercise 6.10 - Added redux toolkit

--- part6/redux-anecdotes/src/components/Filter.jsx
+++ part6/redux-anecdotes/src/components/Filter.jsx
@@ -1,13 +1,14 @@
 import { useDispatch } from 'react-redux'
-import { setFilter } from '../reducers/filterReducer'
-
 const Filter = () => {
   const dispatch = useDispatch()
 
   const handleChange = (event) => {
     // input-field value is in variable event.target.value
     const filter = event.target.value
-    dispatch(setFilter(filter))
+    dispatch({
+      type: 'filter/setFilter',
+      payload: filter
+    })
   }
   const style = {
     marginBottom: 10
