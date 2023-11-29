27e9cce127608895e32ba3847d3a47089e8b18d3
Leandiv <leandro.divito@gmail.com>
2023-11-29 19:41:04 -0300

    Part 8 - Exercise 8.11 - Let the user set author born year

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,4 +1,4 @@
-import {gql, useQuery} from "@apollo/client";
+import {gql, useMutation, useQuery} from "@apollo/client";
 
 const ALL_AUTHORS = gql`
   query {
@@ -10,14 +10,44 @@ const ALL_AUTHORS = gql`
   }
 `
 
+export const EDIT_AUTHOR = gql`
+  mutation editAuthor($name: String!, $setBornTo: Int!) {
+    editAuthor(
+    name: $name,
+    setBornTo: $setBornTo
+  ) {
+    name
+    born
+    bookCount
+  }
+}
+`
+
 const Authors = () => {
   const result = useQuery(ALL_AUTHORS)
+
+  const [changeBorn] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [ { query: ALL_AUTHORS } ]
+  })
 
   if (result.loading) {
     return <div>loading...</div>
   }
 
   const authors = result.data.allAuthors
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    const name = event.target.name.value
+    const setBornTo = event.target.born.value
+
+    console.log('change born...')
+    await changeBorn({ variables: { name, setBornTo : Number(setBornTo) } })
+
+    event.target.name.value = ''
+    event.target.born.value = ''
+  }
 
   return (
     <div>
@@ -36,13 +66,32 @@ const Authors = () => {
         {authors.map(a =>
           <tr key={a.name}>
             <td>{a.name}</td>
-            <td>{a.born ? a.born : null}</td>
+            <td>{a.born}</td>
             <td>{a.bookCount}</td>
           </tr>
         )}
         </tbody>
       </table>
+
+      <h2>Set birthyear</h2>
+      <form onSubmit={submit}>
+        <div>
+          name
+          <input
+            name='name'
+          />
+        </div>
+        <div>
+          born
+          <input
+            name='born'
+          />
+        </div>
+        <button type='submit'>update author</button>
+      </form>
     </div>
   )
+
 }
+
 export default Authors
