8421c0a59ef311e3a21bcf9d6b305b53aed21656
Leandiv <leandro.divito@gmail.com>
2023-12-01 01:01:10 -0300

    Part 8 - Exercise 8.18 - Add user

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -2,9 +2,50 @@ import { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import Login from './components/Login'
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
+import {useApolloClient} from "@apollo/client";
 
 const App = () => {
+  const [token, setToken] = useState(null)
+  const client = useApolloClient()
+
+  // Check if user is logged in
+  if (!token && localStorage.getItem('library-user-token')) {
+    setToken(localStorage.getItem('library-user-token'))
+  }
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  if (!token) {
+    return (
+      <div>
+        <Router>
+          <div>
+            <Link to="/authors">
+              <button>authors</button>
+            </Link>
+            <Link to="/books">
+              <button>books</button>
+            </Link>
+            <Link to={"/login"}>
+              <button>login</button>
+            </Link>
+          </div>
+
+          <Routes>
+            <Route path="/authors" element={<Authors />} />
+            <Route path="/books" element={<Books />} />
+            <Route path="/login" element={<Login setToken={setToken} />} />
+          </Routes>
+        </Router>
+      </div>
+    )
+  }
 
   return (
     <div>
@@ -19,6 +60,7 @@ const App = () => {
           <Link to="/add">
             <button>add book</button>
           </Link>
+          <button onClick={logout}>logout</button>
         </div>
 
         <Routes>
