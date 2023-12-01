28ba259913e0906ab004bfb1f329bc16c3cad9d8
Leandiv <leandro.divito@gmail.com>
2023-12-01 00:36:09 -0300

    Part 8 - Exercise 8.17 - Fixed books page

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -4,7 +4,9 @@ const ALL_BOOKS = gql`
   query {
     allBooks {
       title
-      author
+      author {
+        name
+      }
       published
     }
   }
@@ -36,7 +38,7 @@ const Books = () => {
         {books.map(b =>
           <tr key={b.title}>
             <td>{b.title}</td>
-            <td>{b.author}</td>
+            <td>{b.author.name}</td>
             <td>{b.published}</td>
           </tr>
         )}
