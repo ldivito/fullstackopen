4a4da3b8e36512f59dd74944213fe9f5e11efef1
Leandiv <leandro.divito@gmail.com>
2023-12-03 23:49:50 -0300

    Part 9 - 9.2 - Calculate exercise hours

---
+++ part9/typescript/exerciseCalculator.ts
@@ -0,0 +1,42 @@
+type Rating = 1 | 2 | 3;
+
+type RatingDescription = 'bad' | 'not too bad but could be better' | 'good';
+
+
+
+
+interface Result {
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: Rating,
+    ratingDescription: RatingDescription,
+    target: number,
+    average: number
+}
+
+const exerciseCalculator = (target: number, hours: Array<number>): Result => {
+    const periodLength = hours.length;
+    const trainingDays = hours.filter(h => h > 0).length;
+    const average = hours.reduce((a, b) => a + b) / periodLength;
+    const success = average >= target;
+    const rating = average >= target ? 3 : average >= target / 2 ? 2 : 1;
+    const ratingDescription = rating === 3 ? 'good' : rating === 2 ? 'not too bad but could be better' : 'bad';
+    const targetHours = target;
+
+    return {
+        periodLength,
+        trainingDays,
+        success,
+        rating,
+        ratingDescription,
+        target: targetHours,
+        average
+    };
+}
+
+
+const target = 12;
+const hours = [3, 0, 2, 4.5, 0, 3, 1];
+
+console.log(exerciseCalculator(target, hours));
