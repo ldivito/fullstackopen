be6a70edb97b44c870c4b95940ce7ead14d97b40
Leandiv <leandro.divito@gmail.com>
2023-12-05 23:35:57 -0300

    Part 9 - 9.10 - Add diagnoses endpoint

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -5,6 +5,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+import diagnosesRouter from './src/routes/diagnosesRoute';
+
 const PORT = 3001;
 
 const baseUrl = '/api';
@@ -13,6 +15,8 @@ app.get(baseUrl + '/ping', (_req, res) => {
 	console.log('someone pinged here');
 	res.send('pong');
 });
+
+app.use(baseUrl + '/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
